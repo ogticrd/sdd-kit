7eacffc4934454a4797bd5355999eba96a5cce95
Christian De La Cruz <delacruzfamilia13@gmail.com>
2023-09-05 13:25:02 -0400

    start:
    Start project with basic configuration

---
+++ .storybook/preview.ts
@@ -0,0 +1,38 @@
+import type { Preview } from "@storybook/react";
+// .storybook/preview.js
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/material-icons";
+
+import { withThemeByClassName } from "@storybook/addon-styling";
+
+import "../index.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+
+  decorators: [
+    // Adds theme switching support.
+    // NOTE: requires setting "darkMode" to "class" in your tailwind config
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
+};
+
+export default preview;
