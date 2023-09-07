3bf08d0dc977918c525074309fb863459ad99b44
Christian De La Cruz <delacruzfamilia13@gmail.com>
2023-09-07 11:02:20 -0400

    [feat] Add configuration for npmjs
    Full configuration for deploying the library to npmjs

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export * from "./button";
+export * from "./header";
+export * from "./loading";
+export * from "./loadingBackdrop";
---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+import packageJson from "./package.json" assert { type: "json" };
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+
+export default [
+  {
+    input: "index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      PeerDepsExternalPlugin(), //<--- THIS
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
