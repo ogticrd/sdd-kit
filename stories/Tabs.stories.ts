80c9da4c0bad064fe1eadc2ce81b6aa5d8aa6d06
Christian De La Cruz <delacruzfamilia13@gmail.com>
2023-09-08 09:59:43 -0400

    [ADD] Parameters in tabs

--- stories/Tabs.stories.ts
+++ stories/Tabs.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Tabs } from "../components/tabs";
+import React from "react";
 
 const meta = {
   title: "Example/Tabs",
@@ -12,6 +13,13 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const tab1Content = React.createElement("div", null, "Tab 1 content");
+const tab2Content = React.createElement("div", null, "Tab 2 content");
+const tab3Content = React.createElement("div", null, "Tab 3 content");
+
 export const playground: Story = {
-  args: {},
+  args: {
+    titles: ["Tab 1", "Tab 2", "Tab 3"],
+    children: [tab1Content, tab2Content, tab3Content],
+  },
 };
