a80bc3527b9b16e713e674634ed27cd1dd741f07
Tomas Familia <tomas.familia@outlookcom>
2023-09-11 14:10:00 -0400

    Recreated the project in Next.js

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,22 +1,15 @@
-import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import type { Preview } from "@storybook/react";
 
-import { theme } from "../theme";
-
-import { CssBaseline, ThemeProvider } from "@mui/material";
-
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/material-icons";
-
-export const decorators = [
-  withThemeFromJSXProvider({
-    themes: {
-      light: theme,
-      dark: theme,
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
-    Provider: ThemeProvider,
-    GlobalStyles: CssBaseline,
-  }),
-];
+  },
+};
+
+export default preview;
