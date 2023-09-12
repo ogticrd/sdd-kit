3e79f9eee565ef7889a45ac20e7879a698c0cf23
je1999 <je99alvarez@gmail.com>
2023-09-12 15:43:03 -0400

    fix: duplicate react import

--- stories/Card.stories.ts
+++ stories/Card.stories.ts
@@ -2,7 +2,6 @@ import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Card } from "../components/card";
-import React from "react";
 import { Button } from "../components/button";
 
 const meta = {
