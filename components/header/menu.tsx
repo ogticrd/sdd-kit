83cc333f2d0c8456f1548f3e0c1f78006f0a1692
Christian De La Cruz <delacruzfamilia13@gmail.com>
2023-09-14 11:17:02 -0400

    feat: :lipstick: Add floating menu to header

--- components/header/menu.tsx
+++ components/header/menu.tsx
@@ -6,18 +6,17 @@ export default function HeaderMenu({ menuItems, isMenuOpen }: { menuItems: IMenu
 
 
     return (
-        <div className='bg-white border-y border-y-gray-200'>
+        <div className='bg-white border-y border-y-gray-200 relative'>
             <nav
-                className={`bg-white flex md:items-center gap-8 container mx-auto my-4 px-4
-                    ${isMenuOpen ? 'flex flex-col sm:justify-start sm:items-start' : 'hidden md:flex md:flex-row'}
-
-            `}>
+                className={`bg-white flex md:items-center gap-8 container mx-auto my-4 px-4 xl:relative
+                    ${isMenuOpen ? 'flex flex-col sm:justify-start sm:items-start absolute -top-4 border w-screen -left-4 mx-auto py-4 shadow-md' : 'hidden md:flex md:flex-row'}
+                    `}>
                 {
                     menuItems.map((item, index) =>
                         <Link
                             href={item.path}
                             key={index}
-                            className='text-blue-900'
+                            className='text-blue-900 hover:text-blue-500'
                             underline='none'
                         >
                             {item.name}
