d5fae6cb02a61af1f1c1b3516af49df4ec087988
Christian De La Cruz <delacruzfamilia13@gmail.com>
2023-10-18 21:52:20 -0400

    feat: Add params to select components
    Add the params required by select component like options, label, etc

--- stories/Select.stories.ts
+++ stories/Select.stories.ts
@@ -12,14 +12,32 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const options = [
+  {
+    label: "Option 1",
+    value: "option1",
+  },
+  {
+    label: "Option 2",
+    value: "option2",
+  },
+  {
+    label: "Option 3",
+    value: "option3",
+  },
+  {
+    label: "Option 4",
+    value: "option4",
+  },
+];
+
 export const playground: Story = {
   args: {
-    // id: "id",
-    // label: "Nombre",
-    // variant: "outlined",
-    // color: "primary",
-    // args: {
-    //   size: "small",
-    // },
+    options: options,
+    label: "Select an option",
+    value: "option1",
+    onChange: (v) => {
+      console.log(v);
+    },
   },
 };
