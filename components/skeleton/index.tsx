e34aed1dd7098b5191e81d48dc4ad12257aad2f3
je1999 <je99alvarez@gmail.com>
2024-04-04 01:19:51 -0400

    feat: Add parameters to Skeleton component

--- components/skeleton/index.tsx
+++ components/skeleton/index.tsx
@@ -1,24 +1,15 @@
 import * as React from 'react';
 import MUISkeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
 
-export interface ISkeletonProps {
+export interface IPropsSkeleton {
   variant?: 'text' | 'circular' | 'rectangular' | 'rounded'
   width?: number
   height?: number
-  fontSize?: string
 }
 
-export const Skeleton = ({ variant = 'text' }: ISkeletonProps) => {
+export const Skeleton = ({ variant = 'text', width, height }: IPropsSkeleton) => {
   return (
-    <Stack spacing={1}>
-      <MUISkeleton variant="text" sx={{ fontSize: '1rem' }} />
-
-      {/* For other variants, adjust the size with `width` and `height` */}
-      <MUISkeleton variant="circular" width={40} height={40} />
-      <MUISkeleton variant="rectangular" width={210} height={60} />
-      <MUISkeleton variant="rounded" width={210} height={60} />
-    </Stack>
+    <MUISkeleton variant={variant} width={width} height={height} />
   );
 }
 
