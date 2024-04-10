2ebcbca47abd49010a2e82a73bd536d687b569fc
Victor1890 <victorrosariodeveloper@gmail.com>
2024-04-10 14:15:27 -0400

    fix: :green_heart: fix import paths and add missing code in Image component
    fix import paths and solve problem to image ref property type

--- components/image/image.interface.ts
+++ components/image/image.interface.ts
@@ -1,10 +1,10 @@
-import { ImgHTMLAttributes } from "react";
+import { ImgHTMLAttributes, Ref } from "react";
 import { ImageVariantProps } from "./image.style";
 
 type NativeImageProps = ImgHTMLAttributes<HTMLImageElement>;
 
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-    ref?: React.RefObject<HTMLImageElement | null>
+    ref?: Ref<HTMLImageElement> | undefined
     isBlurred?: boolean;
     fallbackSrc?: React.ReactNode;
     disableSkeleton?: boolean;
@@ -13,20 +13,6 @@ interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
     removeWrapper?: boolean;
     isLoading?: boolean;
     onError?: () => void;
-    /**
-     * Classname or List of classes to change the classNames of the element.
-     * if `className` is passed, it will be added to the base slot.
-     *
-     * @example
-     * ```ts
-     * <Image classNames={{
-     *    base:"base-classes", // image classes
-     *    wrapper: "wrapper-classes",
-     *    blurredImg: "blurredImg-classes", // this is a cloned version of the img
-     * }} />
-     * ```
-     */
-    // classNames?: SlotsToClasses<ImageSlots>;
 }
 
 export type UseImageProps = ImageProps & ImageVariantProps;
